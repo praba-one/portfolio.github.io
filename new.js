84b235d584b179551556253ebeb9cf7575f800a9
praba-one <144136391+praba-one@users.noreply.github.com>
2023-09-05 19:09:39 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,11 @@
+var button=document.getElementById("button");
+var h1=document.getElementById("h1");
+
+// button.addEventListener("click",function(){
+//     h1.innerHTML= Date();
+
+// })
+
+function showdate(){
+    h1.innerHTML=Date();
+}
